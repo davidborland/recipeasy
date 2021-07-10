a2c2aaff85cd8304257a946b5b864228b9052bca
David Borland <borland@renci.org>
2021-07-10 16:31:11 -0400

    Initial working version

--- recipeasy.js
+++ recipeasy.js
@@ -1,5 +1,23 @@
+var title = document.createElement("h1");
+title.textContent = document.getElementsByTagName("title")[0].textContent;
+
+var ingredientsHeader = document.createElement("h3");
+ingredientsHeader.textContent = "Ingredients";
+
+var ingredients = document.getElementsByClassName("recipe-ingredients")[0];
+ingredients.className = "";
+
+var stepsHeader = document.createElement("h3");
+stepsHeader.textContent = "Steps";
+
+var steps = document.getElementsByClassName("recipe-steps")[0];
+steps.className = "";
+
+document.head.textContent = "";
 document.body.textContent = "";
 
-var header = document.createElement('h1');
-header.textContent = "This page has been eaten";
-document.body.appendChild(header);
+document.body.appendChild(title);
+document.body.appendChild(ingredientsHeader);
+document.body.appendChild(ingredients);
+document.body.appendChild(stepsHeader);
+document.body.appendChild(steps);
