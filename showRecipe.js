4d58b05ed1333ca96efc1bd370aeef30d50851b4
David Borland <borland@renci.org>
2021-09-18 10:02:21 -0400

    Works with multiple ingredient and step groups

--- showRecipe.js
+++ showRecipe.js
@@ -1,23 +1,36 @@
+function copyElements(collection) {
+    var a = [];
+    for (let item of collection) {
+        a.push(item);
+    }
+
+    return a;
+}
+
 var title = document.createElement("h1");
 title.textContent = document.getElementsByTagName("title")[0].textContent;
 
 var ingredientsHeader = document.createElement("h3");
 ingredientsHeader.textContent = "Ingredients";
 
-var ingredients = document.getElementsByClassName("recipe-ingredients")[0];
-ingredients.className = "";
+var ingredients = copyElements(document.getElementsByClassName("recipe-ingredients"));
+ingredients.forEach(d => d.className = "");
 
 var stepsHeader = document.createElement("h3");
 stepsHeader.textContent = "Steps";
 
-var steps = document.getElementsByClassName("recipe-steps")[0];
-steps.className = "";
+var steps = copyElements(document.getElementsByClassName("recipe-steps"));
+steps.forEach(d => d.className = "");
 
 document.head.textContent = "";
 document.body.textContent = "";
 
 document.body.appendChild(title);
 document.body.appendChild(ingredientsHeader);
-document.body.appendChild(ingredients);
+for (let ingredient of ingredients) {
+    document.body.appendChild(ingredient);
+}
 document.body.appendChild(stepsHeader);
-document.body.appendChild(steps);
+for (let step of steps) {
+    document.body.appendChild(step);
+}
